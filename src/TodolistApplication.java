a415513f3561c99861ec9cc097ce0b7594661d78
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-13 09:41:47 +0800

    test show todo list

--- src/TodolistApplication.java
+++ src/TodolistApplication.java
@@ -3,20 +3,33 @@ public class TodolistApplication {
   public static String[] model = new String[10];
 
   public static void main(String[] args) {
-
+    testShowTodolist();
   }
 
   /**
    * Menampilkan todo list
    */
   public static void showTodoList() {
+    for (int i = 0; i < model.length; i++) {
+      String todo = model[i];
+      int no = i + 1;
 
+      if (todo != null) {
+        System.out.println(no + ". " + todo);
+      }
+    }
+  }
+
+  public static void testShowTodolist() {
+    model[0] = "Belajar Java Dasar";
+    model[1] = "Studi Kasus Java Dasar : Aplikasi Todolist";
+    showTodoList();
   }
 
   /**
    * Menambah todo ke list
    */
-  public static void addTodoList() {
+  public static void addTodoList(String todo) {
 
   }
 
